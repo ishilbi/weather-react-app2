5466e0cf2e7d562bfc18f18d8e0207ea7f901645
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-16 13:11:50 -0400

    Added GitHub repo Url and profile url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://eclectic-pie-9d7f2a.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Silvia Lee
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/ishilbi/weather-react-app2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

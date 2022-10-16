32d60ba0491801d4448a1b49e6d56a64e29f6c7c
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-16 14:00:06 -0400

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://eclectic-pie-9d7f2a.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Silvia Lee
-        </a>{" "}
-        and is open sourced on{" "}
-        <a
-          href="https://github.com/ishilbi/weather-react-app2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://eclectic-pie-9d7f2a.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Silvia Lee
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/ishilbi/weather-react-app2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

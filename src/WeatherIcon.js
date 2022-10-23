dab52259d8327e669da3c070935f6aa5fe32527c
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-22 21:57:34 -0400

    Added WeatherForecast component and css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,8 +26,8 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="rgb(33, 33, 33)"
-      size={64}
+      color="#1e1e1e"
+      size={props.size}
       animate={true}
     />
   );

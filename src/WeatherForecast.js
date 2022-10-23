9168b47d03ea136c538da65c5f1cc12ae438280c
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-22 22:21:41 -0400

    added extra columns to the weatherForecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,9 +5,41 @@ import "./WeatherForecast.css";
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
-      <div className="row">
+      <div className="row mt-4">
         <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
+          <div className="WeatherForecast-days">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-days">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>{" "}
+        <div className="col">
+          <div className="WeatherForecast-days">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>{" "}
+        <div className="col">
+          <div className="WeatherForecast-days">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>{" "}
+        <div className="col">
+          <div className="WeatherForecast-days">Thu</div>
           <WeatherIcon code="01d" size={36} />
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
